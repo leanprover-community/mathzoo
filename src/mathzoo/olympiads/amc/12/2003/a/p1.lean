7b830a22cbf6534f28e6e686177c3c44e30b5523
Stanislas Polu <spolu@openai.com>
2022-03-11 17:06:56 +0100

    gem: amc12a 2003 p1 (gpt-f)

--- src/mathzoo/olympiads/amc/12/2003/a/p1.lean
+++ src/mathzoo/olympiads/amc/12/2003/a/p1.lean
@@ -7,8 +7,18 @@ import mathzoo.imports.miniF2F
 
 open_locale nat rat real big_operators topological_space
 
-axiom amc12a_2003_p1
+/--
+What is the difference between the sum of the first 2003 even counting 
+numbers and the sum of the first 2003 odd counting numbers?
+Answer: 2003
+--/
+theorem amc12a_2003_p1
   (u v : ℕ → ℕ)
   (h₀ : ∀ n, u n = 2 * n + 2)
   (h₁ : ∀ n, v n = 2 * n + 1) :
-  (∑ k in finset.range(2003), u k) - (∑ k in finset.range(2003), v k) = 2003 
+  (∑ k in finset.range(2003), u k) - (∑ k in finset.range(2003), v k) = 2003 :=
+begin
+  simp [finset.range, h₀, h₁],
+  simp [*, nat.add_sub_add_left, mul_comm],
+  norm_num [h₁],
+end
