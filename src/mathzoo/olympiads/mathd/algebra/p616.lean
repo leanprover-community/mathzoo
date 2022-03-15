c6b328b7728c9e2116b36f18e2ff95f3dbe744fa
Stanislas Polu <spolu@openai.com>
2022-03-15 16:11:00 +0100

    gem: mathd algebra 616 (gpt-f)

--- src/mathzoo/olympiads/mathd/algebra/p616.lean
+++ src/mathzoo/olympiads/mathd/algebra/p616.lean
@@ -7,8 +7,15 @@ import mathzoo.imports.miniF2F
 
 open_locale nat rat real big_operators topological_space
 
-axiom mathd_algebra_616
+/--
+Given the two functions $f(x)=x^3+2x+1$ and $g(x)=x-1$, find $f(g(1))$.
+Answer: $1$.
+--/
+theorem mathd_algebra_616
   (f g : ℝ → ℝ)
   (h₀ : ∀ x, f x = x^3 + 2 * x + 1)
   (h₁ : ∀ x, g x = x - 1) :
-  f (g 1) = 1 
+  f (g 1) = 1 :=
+begin
+  simp [h₁, h₀],
+end
