359dcfe7cd6f6e35d70ab3e8d7e51f1d17cc9d20
Stanislas Polu <spolu@openai.com>
2022-03-15 16:21:58 +0100

    gem: MATH numbertheory 149 (gpt-f)

--- src/mathzoo/olympiads/mathd/numbertheory/p149.lean
+++ src/mathzoo/olympiads/mathd/numbertheory/p149.lean
@@ -7,5 +7,15 @@ import mathzoo.imports.miniF2F
 
 open_locale nat rat real big_operators topological_space
 
-axiom mathd_numbertheory_149 :
-  ∑ k in (finset.filter (λ x, x % 8 = 5 ∧ x % 6 = 3) (finset.range 50)), k = 66 
+/--
+A group of $N$ students, where $N < 50$, is on a field trip.
+If their teacher puts them in groups of 8, the last group has 5 students.
+If their teacher instead puts them in groups of 6, the last group has 3 students.
+What is the sum of all possible values of $N$?
+Answer: $66$.
+--/
+theorem mathd_numbertheory_149 :
+  ∑ k in (finset.filter (λ x, x % 8 = 5 ∧ x % 6 = 3) (finset.range 50)), k = 66 :=
+begin
+  congr',
+end
