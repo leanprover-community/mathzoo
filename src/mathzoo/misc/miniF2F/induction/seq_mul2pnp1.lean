bbee1d70bc2f4c7ecb86e81819e1bcf046effa03
Stanislas Polu <spolu@openai.com>
2022-03-15 16:32:38 +0100

    gem: induction_seq_mul2pnp1 (gpt-f)

--- src/mathzoo/misc/miniF2F/induction/seq_mul2pnp1.lean
+++ src/mathzoo/misc/miniF2F/induction/seq_mul2pnp1.lean
@@ -7,9 +7,21 @@ import mathzoo.imports.miniF2F
 
 open_locale nat rat real big_operators topological_space
 
-axiom induction_seq_mul2pnp1
+theorem induction_seq_mul2pnp1
   (n : ℕ)
   (u : ℕ → ℕ)
   (h₀ : u 0 = 0)
   (h₁ : ∀ n, u (n + 1) = 2 * u n + (n + 1)) :
-  u n = 2^(n + 1) - (n + 2) 
+  u n = 2^(n + 1) - (n + 2) :=
+begin
+  apply eq_tsub_of_add_eq,
+  simp [pow_succ],
+  induction n with a ih,
+  { simp [h₀, h₁] },
+  norm_num [h₀, h₁],
+  simp [nat.succ_eq_add_one],
+  rw [add_left_comm, ← add_assoc],
+  simp only [pow_succ'],
+  rw [mul_two],
+  linarith,
+end
