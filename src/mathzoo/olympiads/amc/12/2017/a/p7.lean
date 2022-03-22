19662c8b044be1fd1e27a1ced77ea8e8af9764bd
Stanislas Polu <spolu@openai.com>
2022-03-22 11:36:19 +0100

    fix: amc12a_2017_p7

--- src/mathzoo/olympiads/amc/12/2017/a/p7.lean
+++ src/mathzoo/olympiads/amc/12/2017/a/p7.lean
@@ -7,13 +7,9 @@ import mathzoo.imports.miniF2F
 
 open_locale nat rat real big_operators topological_space
 
-theorem amc12a_2017_p7
+axiom amc12a_2017_p7
   (f : ℕ → ℝ)
   (h₀ : f 1 = 2)
-  (h₁ : ∀ n, even n → f n = f (n - 1) + 1)
-  (h₂ : ∀ n, odd n → f n = f (n - 2) + 1) :
-  f 2017 = 2018 :=
-begin
-  have t := h₁ 0,
-  norm_num at t,
-end
+  (h₁ : ∀ n, 1 < n ∧ even n → f n = f (n - 1) + 1)
+  (h₂ : ∀ n, 1 < n ∧ odd n → f n = f (n - 2) + 2) :
+  f 2017 = 2018
